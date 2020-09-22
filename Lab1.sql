7ad76a6b4715b506a6199f9816ced6b6583ba699
tech2022 <71337297+tech2022@users.noreply.github.com>
2020-09-22 18:34:49 -0400

    Add files via upload

---
+++ Lab1.sql
@@ -0,0 +1,9 @@
+
+select sakila.film.film_id ,sakila.film.title, sakila.film_category.category_id  from sakila.film inner join sakila.film_category  on sakila.film.film_id= sakila.film_category.film_id ;
+
+
+
+
+
+
+
